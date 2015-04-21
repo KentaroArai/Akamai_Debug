6c52a9788290f4b8798f95375c78ce61c9f322b6
karai <kentaro.arai@gmail.com>
2015-04-21 18:46:07 +0900

    Add Comment for URL

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ debug_headers = ("akamai-x-cache-on",
                  "akamai-x-get-true-cache-key",
                  "akamai-x-serial-no")
 
+# This is test use
 base_url = r"http://downloadcenter.samsung.com/content/SW/201211/20121129051929115/Samsung_USB_Driver_for_Mobile_Phones_v1.5.14.0.exe"
 
 
